6d31ce52d482c5dc0cc8313ad3a12e18acca0ace
spez1998 <samman30@live.com>
2022-02-13 17:07:02 +0000

    Written strucutre of full subBytes module

--- rtl/top.vhd
+++ rtl/top.vhd
@@ -9,7 +9,7 @@ use IEEE.std_logic_1164.all;
 entity top is
     port(inByte:  std_logic_vector(7 downto 0),
          outByte: std_logic_vector(7 downto 0),
-         enc_dec: std_logic);
+         enc_dec: std_logic); -- 0 for ENC, 1 for DEC
 end entity;
 
 architecture arch of top is
@@ -18,22 +18,51 @@ architecture arch of top is
              outByte : out std_logic_vector (7 downto 0));
     end component;
 
-    component rawToGF_Forward
-        port(inByte  : in std_logic_vector(7 downto 0);
-             outByte : out std_logic_vector(7 downto 0));
+    component affine
+        port(inByte  : in std_logic_vector (7 downto 0);
+             outByte : out std_logic_vector (7 downto 0));
     end component;
 
-    component GFtoRaw_Forward
-        port(inByte  : in std_logic_vector(7 downto 0);
-             outByte : out std_logic_vector(7 downto 0));
+    component inverseAffine
+        port(inByte  : in std_logic_vector (7 downto 0);
+             outByte : out std_logic_vector (7 downto 0));
     end component;
 
-    component rawToGF_Backward
-    port(inByte  : in std_logic_vector(7 downto 0);
-         outByte : out std_logic_vector(7 downto 0));
+    component iso
+        port(inByte  : in std_logic_vector (7 downto 0);
+             outByte : out std_logic_vector (7 downto 0));
     end component;
 
-    component GFtoRaw_Backward
-        port(inByte  : in std_logic_vector(7 downto 0);
-            outByte : out std_logic_vector(7 downto 0));
+    component inverseIso
+        port(inByte  : in std_logic_vector (7 downto 0);
+             outByte : out std_logic_vector (7 downto 0));
     end component;
+
+    signal isoIn:  std_logic_vector(7 downto 0);
+    signal invIsoOut: std_logic_vector(7 downto 0);
+    signal multIn:  std_logic_vector(7 downto 0);
+    signal multOut: std_logic_vector(7 downto 0);
+    signal decInvAffine: std_logic_vector(7 downto 0);
+    signal invAffineOut: std_logic_vector(7 downto 0);
+    signal encAffine:    std_logic_vector(7 downto 0);
+    signal affineOut:    std_logic_vector(7 downto 0);
+
+    begin
+        
+        inverseAffine: inverseAffine port map(inByte => inByte, outByte => invAffineOut);
+        affine: affine port map(inByte => invIsoOut, outByte => affineOut);
+        iso: iso port map(inByte => isoIn, outByte => multIn);
+        inverseIso: inverseIso port map(inByte => multOut, outByte => invIsoOut);
+        multInv: multInv port map(inByte => multIn; outByte => multOut);
+
+        process(inByte, outByte, enc_dec) begin
+            if(enc_dec == "0") then
+                isoIn  <= inByte;
+                outByte <= AffineOut;
+            else
+                isoIn  <= invAffineOut;
+                outByte <= invIsoOut;
+            end if;
+        end process;
+
+end arch;
