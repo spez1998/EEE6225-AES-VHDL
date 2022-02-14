0a8fb808c76595fc7ef0a7a157a8828e579b325f
spez1998 <samman30@live.com>
2022-02-14 14:29:57 +0000

    Testbench writes 00000000 to 11111111 to DUT

--- tb/subbytes.vhd
+++ tb/subbytes.vhd
@@ -1,5 +1,6 @@
-library ieee;
-use ieee.std_logic_1164.all;
+-- Code your testbench here
+library IEEE;
+use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.env.finish;
 
@@ -7,42 +8,29 @@ entity subbytes_tb is
 end subbytes_tb;
 
 architecture sim of subbytes_tb is
-    signal inByte  : std_logic_vector(7 downto 0) := (others => '0');
-    signal outByte : std_logic_vector(7 downto 0);
-    signal enc_dec : std_logic := '0';
-    
-    begin
-        dut: top port map(inByte => inByte, outByte => outByte, enc_dec => enc_dec);
+	component top
+    	port(
+        	inByte: in std_logic_vector(7 downto 0);
+            outByte: out std_logic_vector(7 downto 0)
+    	);
+	end component;
 
-end architecture;
+    signal inByte, outByte: std_logic_vector(7 downto 0);
 
-PROC_SEQUENCE : process
-begin
+	begin
 
-    -- Test all possible input values
-    for i in 0 to 2**inByte'length - 1 loop
-        inByte <= std_logic_vector(to_unsigned(i, inByte'length));
-        wait for 10 ns;
-    end loop;
+    	dut: pthru port map(inByte => inByte, outByte => outByte);
 
-    -- Finally, test the wrapped value
-    inByte <= (others => '0');
-    wait for 10 ns;
+        PROC_SEQUENCE: process
+        begin
 
-    report "Test: OK";
-    finish;
+            for j in 0 to 2**inByte'length - 1 loop
+                inByte <= std_logic_vector(to_unsigned(j, inByte'length));
+                wait for 10 ns;
+            end loop;
+            report "Test: OK";
+            finish;
 
-end process;
+        end process;
 
-PROC_CHECKER : process
-    variable prev : std_logic_vector(outByte'range);
-    variable count : integer;
-begin
-    wait on inByte; -- Equivalent to inByte in sensitivity list
-    prev := outByte;
-    wait for 1ns; -- Wait for DUT logic to propagate
-
-    -- TODO: Write logic that checks if outByte is expected according to
-    -- pre-written sBox tables
-
-end process;
+end sim;
