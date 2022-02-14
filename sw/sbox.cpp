d5938e44f388b6e228c411e1907aa33f9cca9c09
spez1998 <samman30@live.com>
2022-02-14 17:40:21 +0000

    Software checker for output of testbench

---
+++ sw/sbox.cpp
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+#define ROTL8(x,shift) ((uint8_t) ((x) << (shift)) | ((x) >> (8 - (shift))))
+
+void initialize_aes_sbox(uint8_t sbox[256]) {
+	uint8_t p = 1, q = 1;
+	
+	/* loop invariant: p * q == 1 in the Galois field */
+	do {
+		/* multiply p by 3 */
+		p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+
+		/* divide q by 3 (equals multiplication by 0xf6) */
+		q ^= q << 1;
+		q ^= q << 2;
+		q ^= q << 4;
+		q ^= q & 0x80 ? 0x09 : 0;
+
+		/* compute the affine transformation */
+		uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+
+		sbox[p] = xformed ^ 0x63;
+	} while (p != 1);
+
+	/* 0 is a special case since it has no inverse */
+	sbox[0] = 0x63;
+}
