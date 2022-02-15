119da5e0bd289687e5d2592db078138d7f4baad7
spez1998 <samman30@live.com>
2022-02-15 13:44:40 +0000

    Simplified 2-bit XORing

--- rtl/gf4mult.vhd
+++ rtl/gf4mult.vhd
@@ -17,10 +17,7 @@ architecture struct of gf4mult is
              outQ : out std_logic_vector (1 downto 0));
     end component;
     
-    signal gf2mult2_b3 : std_logic;
-    signal gf2mult2_b2 : std_logic;
-    signal gf2mult2_b1 : std_logic;
-    signal gf2mult2_b0 : std_logic;
+    signal gf2mult2_in : std_logic_vector(3 downto 0);
 
     signal gf2mult1_out : std_logic_vector(1 downto 0);
     signal gf2mult2_out : std_logic_vector(1 downto 0);
@@ -29,29 +26,21 @@ architecture struct of gf4mult is
     signal phi_out : std_logic_vector(1 downto 0);
 
     begin
-        gf2mult2_b3 <= inByte(7) xor inByte(5);
-        gf2mult2_b2 <= inByte(6) xor inByte(4);
-        gf2mult2_b1 <= inByte(3) xor inByte(1);
-        gf2mult2_b0 <= inByte(2) xor inByte(0);
-        
+        gf2mult2_in(3 downto 2) <= inByte(7 downto 6) xor inByte(5 downto 4);
+        gf2mult2_in(1 downto 0) <= inByte(3 downto 2) xor inByte(1 downto 0);
         gf2mult1: gf2mult port map(inNib(3 downto 2) => inByte(7 downto 6),
                                    inNib(1 downto 0) => inByte(3 downto 2),
                                    outNib => gf2mult1_out);
-        gf2mult2: gf2mult port map(inNib(3) => gf2mult2_b3,
-                                   inNib(2) => gf2mult2_b2,
-                                   inNib(1) => gf2mult2_b1,
-                                   inNib(0) => gf2mult2_b0,
+        gf2mult2: gf2mult port map(inNib => gf2mult2_in,
                                    outNib => gf2mult2_out);
         gf2mult3: gf2mult port map(inNib(3 downto 2) => inByte(5 downto 4),
                                   inNib(1 downto 0) => inByte(1 downto 0),
                                   outNib => gf2mult3_out);
                                    
         phi1: phi port map(inQ => gf2mult1_out,
-                          outQ => phi_out);
+                           outQ => phi_out);
 
-        outByte(3) <= gf2mult2_out(1) xor gf2mult3_out(1);
-        outByte(2) <= gf2mult2_out(0) xor gf2mult3_out(0);
-        outByte(1) <= phi_out(1) xor gf2mult3_out(1);
-        outByte(0) <= phi_out(0) xor gf2mult3_out(0);
+        outByte(3 downto 2) <= gf2mult2_out xor gf2mult3_out;
+        outByte(1 downto 0) <= phi_out xor gf2mult3_out;
     
 end struct;
