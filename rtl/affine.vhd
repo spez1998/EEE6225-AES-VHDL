03d9b3a94e3a34b5024538be0e92b1254c931bda
spez1998 <samman30@live.com>
2022-02-15 11:11:12 +0000

    Fixed mixing of types for XOR

--- rtl/affine.vhd
+++ rtl/affine.vhd
@@ -9,19 +9,19 @@ end affine;
 architecture behav of affine is
     begin
         outByte(7) <= inByte(7) xor inByte(6) xor inByte(5) xor inByte(4) xor 
-                      inByte(3) xor 0;
+                      inByte(3) xor '0';
         outByte(6) <= inByte(6) xor inByte(5) xor inByte(4) xor inByte(3) xor 
-                      inByte(2) xor 1;
+                      inByte(2) xor '1';
         outByte(5) <= inByte(5) xor inByte(4) xor inByte(3) xor inByte(2) xor 
-                      inByte(1) xor 1;
+                      inByte(1) xor '1';
         outByte(4) <= inByte(4) xor inByte(3) xor inByte(2) xor inByte(1) xor
-                      inByte(0) xor 0;
+                      inByte(0) xor '0';
         outByte(3) <= inByte(7) xor inByte(3) xor inByte(2) xor inByte(1) xor 
-                      inByte(0) xor 0;
+                      inByte(0) xor '0';
         outByte(2) <= inByte(7) xor inByte(6) xor inByte(2) xor inByte(1) xor
-                      inByte(0) xor 0;
+                      inByte(0) xor '0';
         outByte(1) <= inByte(7) xor inByte(6) xor inByte(5) xor inByte(1) xor
-                      inByte(0) xor 1;
+                      inByte(0) xor '1';
         outByte(0) <= inByte(7) xor inByte(6) xor inByte(5) xor inByte(4) xor
-                      inByte(0) xor 1;
+                      inByte(0) xor '1';
 end behav;
--- rtl/inverseAffine.vhd
+++ rtl/inverseAffine.vhd
@@ -8,12 +8,12 @@ end inverseAffine;
 
 architecture behav of inverseAffine is
     begin
-        outByte(7) <= inByte(6) xor inByte(4) xor inByte(1) xor 0;
-        outByte(6) <= inByte(5) xor inByte(3) xor inByte(0) xor 1;
-        outByte(5) <= inByte(7) xor inByte(4) xor inByte(2) xor 1;
-        outByte(4) <= inByte(6) xor inByte(3) xor inByte(1) xor 0;
-        outByte(3) <= inByte(5) xor inByte(2) xor inByte(0) xor 0;
-        outByte(2) <= inByte(7) xor inByte(4) xor inByte(1) xor 0;
-        outByte(1) <= inByte(6) xor inByte(3) xor inByte(0) xor 1;
-        outByte(0) <= inByte(7) xor inByte(5) xor inByte(2) xor 1;
+        outByte(7) <= inByte(6) xor inByte(4) xor inByte(1) xor '0';
+        outByte(6) <= inByte(5) xor inByte(3) xor inByte(0) xor '1';
+        outByte(5) <= inByte(7) xor inByte(4) xor inByte(2) xor '1';
+        outByte(4) <= inByte(6) xor inByte(3) xor inByte(1) xor '0';
+        outByte(3) <= inByte(5) xor inByte(2) xor inByte(0) xor '0';
+        outByte(2) <= inByte(7) xor inByte(4) xor inByte(1) xor '0';
+        outByte(1) <= inByte(6) xor inByte(3) xor inByte(0) xor '1';
+        outByte(0) <= inByte(7) xor inByte(5) xor inByte(2) xor '1';
 end behav;
