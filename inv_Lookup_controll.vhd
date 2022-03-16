6013ce69ff24f2a1ab11b1e0c64ce26ad7601699
ChingTung <63977132+ChingTung@users.noreply.github.com>
2022-03-16 14:52:06 +0000

    Update inv_Lookup_controll.vhd

--- inv_Lookup_controll.vhd
+++ inv_Lookup_controll.vhd
@@ -1,15 +1,16 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
-entity Lookup_controll is
+
+entity inv_Lookup_controll is
   Port ( CLK, SCLR, set: in std_logic;
           A : out std_logic_vector(4 downto 0));
-end Lookup_controll;
+end inv_Lookup_controll;
 
-architecture Behavioral of Lookup_controll is
+architecture Behavioral of inv_Lookup_controll is
+
     signal LOOKUP :  std_logic_vector( 3 downto 0):= "0000";
     signal delay :  std_logic_vector( 3 downto 0) := "1011";
 begin
@@ -27,35 +28,35 @@ begin
                         when "0000"=>
                         A <= "01100";
                         when "0001"=>
-                        A <= "01000";
+                        A <= "00000";
                         when "0010"=>
                         A <= "00100";
                         when "0011"=>
-                        A <= "00000";
+                        A <= "01000";
                         when "0100"=>
                         A <= "01100";
                         when "0101"=>
-                        A <= "01000";
+                        A <= "00000";
                         when "0110"=>
                         A <= "00100";
                         when "0111"=>
-                        A <= "10000";
+                        A <= "11000";
                         when "1000"=>
                         A <= "01100";
                         when "1001"=>
-                        A <= "01000";
+                        A <= "00000";
                         when "1010"=>
                         A <= "10100";
                         when "1011"=>
-                        A <= "10000";
+                        A <= "11000";
                         when "1100"=>
                         A <= "01100";
                         when "1101"=>
-                        A <= "11000";
+                        A <= "10000";
                         when "1110"=>
                         A <= "10100";
                         when "1111"=>
-                        A <= "10000";
+                        A <= "11000";
                         when others =>
                         A <= "11111";
                     end case; 
