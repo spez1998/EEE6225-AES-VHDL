bf038a329a6d027a9ccf8288bcd2654607550528
ChingTung <63977132+ChingTung@users.noreply.github.com>
2022-04-15 11:07:07 +0100

    Update Lookup_controll.vhd

--- Lookup_controll.vhd
+++ Lookup_controll.vhd
@@ -3,64 +3,64 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
-entity Lookup_controll is
+entity controll is
   Port ( CLK, SCLR, set: in std_logic;
           A : out std_logic_vector(4 downto 0));
-end Lookup_controll;
+end controll;
 
-architecture Behavioral of Lookup_controll is
-    signal LOOKUP :  std_logic_vector( 3 downto 0):= "0000";
-    signal delay :  std_logic_vector( 3 downto 0) := "1100";
+architecture Behavioral of controll is
+    signal LOOKUP :  std_logic_vector( 4 downto 0):= "00000";
 begin
     process (CLK) begin
-        
+    --RESET statement 
+      if (SCLR = '1') then 
+        A <= "11111";
+        LOOKUP <= "00000";
+        else 
+    -------
         if rising_edge(CLK) then
-                if (delay /= 0) and set = '1' then
-                    delay <= delay - 1;
-                    A <= "11111";                  
-                end if;
-                if (delay = 0) then 
+                if set = '1'  or  LOOKUP /= "00000" then 
+                    LOOKUP <= LOOKUP + 1;  
                     case LOOKUP is
-                        when "0000"=>
+                        --count delay from "00000" to "01011'
+                        when "01100"=>
                         A <= "01100";
-                        when "0001"=>
-                        A <= "01000";
-                        when "0010"=>
+                        when "01101"=>
+                        A <= "01000";    
+                        when "01110"=>
                         A <= "00100";
-                        when "0011"=>
+                        when "01111"=>
                         A <= "00000";
-                        when "0100"=>
+                        when "10000"=>
                         A <= "01100";
-                        when "0101"=>
+                        when "10001"=>
                         A <= "01000";
-                        when "0110"=>
+                        when "10010"=>
                         A <= "00100";
-                        when "0111"=>
+                        when "10011"=>
                         A <= "10000";
-                        when "1000"=>
+                        when "10100"=>
                         A <= "01100";
-                        when "1001"=>
+                        when "10101"=>
                         A <= "01000";
-                        when "1010"=>
+                        when "10110"=>
                         A <= "10100";
-                        when "1011"=>
+                        when "10111"=>
                         A <= "10000";
-                        when "1100"=>
+                        when "11000"=>
                         A <= "01100";
-                        when "1101"=>
+                        when "11001"=>
                         A <= "11000";
-                        when "1110"=>
+                        when "11010"=>
                         A <= "10100";
-                        when "1111"=>
+                        when "11011"=>
                         A <= "10000";
+                        when "11100"=>
                         when others =>
-                        A <= "11111";
-                        delay <= "1100";
+                            A <= "11111";
                     end case; 
-                    if (set /= '0') or (LOOKUP /= "0000") then
-                        LOOKUP <= LOOKUP + 1;
-                    end if;               
-                end if;
+             end if;
         end if;
+       end if;
     end process;
 end Behavioral;
